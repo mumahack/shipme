6b83e3795820a31c6c58e2e9ef263c1c24f89c5b
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-09-09 20:51:18 +0200

    Mapcalculator

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -60,10 +60,8 @@
     <div class="hero-container">
         <div class="wow fadeIn">
 
-
-
-
             <figure id="container" title="spin the truck"></figure>
+
             <script src="js/d3.v3.min.js"></script>
             <script src="js/transmit_js_3dtruck.min.js"></script>
 
@@ -84,7 +82,9 @@
     <div class="container">
 
         <div id="logo" class="pull-left">
-            <a href="#hero"><img src="img/logo.png" alt="" title="" /></img></a>
+
+            <a href="#hero"><img src="img/ShipMeUP_Schriftzug.png" alt="" title="" /></a>
+            <a href="#hero"><img src="img/ShipMeUP_Logo.png" alt="" title="" /></a>
             <!-- Uncomment below if you prefer to use a text image -->
             <!--<h1><a href="#hero">Header 1</a></h1>-->
         </div>
@@ -93,27 +93,6 @@
             <ul class="nav-menu">
                 <li class="menu-active"><a href="#hero">Home</a></li>
                 <li><a href="#about">About Us</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#portfolio">Portfolio</a></li>
-                <li><a href="#testimonials">Testimonials</a></li>
-                <li><a href="#team">Team</a></li>
-                <li class="menu-has-children"><a href="">Drop Down</a>
-                    <ul>
-                        <li><a href="#">Drop Down 1</a></li>
-                        <li class="menu-has-children"><a href="#">Drop Down 2</a>
-                            <ul>
-                                <li><a href="#">Deep Drop Down 1</a></li>
-                                <li><a href="#">Deep Drop Down 2</a></li>
-                                <li><a href="#">Deep Drop Down 3</a></li>
-                                <li><a href="#">Deep Drop Down 4</a></li>
-                                <li><a href="#">Deep Drop Down 5</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Drop Down 3</a></li>
-                        <li><a href="#">Drop Down 4</a></li>
-                        <li><a href="#">Drop Down 5</a></li>
-                    </ul>
-                </li>
                 <li><a href="#contact">Contact Us</a></li>
             </ul>
         </nav><!-- #nav-menu-container -->
@@ -150,282 +129,6 @@
         </div>
     </div>
 </section>
-
-<!--==========================
-  Services Section
-============================-->
-<section id="services">
-    <div class="container wow fadeInUp">
-        <div class="row">
-            <div class="col-md-12">
-                <h3 class="section-title">Our Services</h3>
-                <div class="section-title-divider"></div>
-                <p class="section-description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium</p>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-4 service-item">
-                <div class="service-icon"><i class="fa fa-desktop"></i></div>
-                <h4 class="service-title"><a href="">Lorem Ipsum</a></h4>
-                <p class="service-description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
-            </div>
-            <div class="col-md-4 service-item">
-                <div class="service-icon"><i class="fa fa-bar-chart"></i></div>
-                <h4 class="service-title"><a href="">Dolor Sitema</a></h4>
-                <p class="service-description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
-            </div>
-            <div class="col-md-4 service-item">
-                <div class="service-icon"><i class="fa fa-paper-plane"></i></div>
-                <h4 class="service-title"><a href="">Sed ut perspiciatis</a></h4>
-                <p class="service-description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-            </div>
-            <div class="col-md-4 service-item">
-                <div class="service-icon"><i class="fa fa-photo"></i></div>
-                <h4 class="service-title"><a href="">Magni Dolores</a></h4>
-                <p class="service-description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-            </div>
-            <div class="col-md-4 service-item">
-                <div class="service-icon"><i class="fa fa-road"></i></div>
-                <h4 class="service-title"><a href="">Nemo Enim</a></h4>
-                <p class="service-description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
-            </div>
-            <div class="col-md-4 service-item">
-                <div class="service-icon"><i class="fa fa-shopping-bag"></i></div>
-                <h4 class="service-title"><a href="">Eiusmod Tempor</a></h4>
-                <p class="service-description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
-            </div>
-        </div>
-    </div>
-</section>
-
-<!--==========================
-  Subscrbe Section
-============================-->
-<section id="subscribe">
-    <div class="container wow fadeInUp">
-        <div class="row">
-            <div class="col-md-8">
-                <h3 class="subscribe-title">Subscribe For Updates</h3>
-                <p class="subscribe-text">Join our 1000+ subscribers and get access to the latest tools, freebies, product announcements and much more!</p>
-            </div>
-            <div class="col-md-4 subscribe-btn-container">
-                <a class="subscribe-btn" href="#">Subscribe Now</a>
-            </div>
-        </div>
-    </div>
-</section>
-
-<!--==========================
-  Porfolio Section
-============================-->
-<section id="portfolio">
-    <div class="container wow fadeInUp">
-        <div class="row">
-            <div class="col-md-12">
-                <h3 class="section-title">Portfolio</h3>
-                <div class="section-title-divider"></div>
-                <p class="section-description">Si stante, hoc natura videlicet vult, salvam esse se, quod concedimus ses haec dicturum fuisse</p>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-3">
-                <a class="portfolio-item" style="background-image: url(img/portfolio-1.jpg);" href="">
-                    <div class="details">
-                        <h4>Portfolio 1</h4>
-                        <span>Alored dono par</span>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a class="portfolio-item" style="background-image: url(img/portfolio-2.jpg);" href="">
-                    <div class="details">
-                        <h4>Portfolio 2</h4>
-                        <span>Alored dono par</span>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a class="portfolio-item" style="background-image: url(img/portfolio-3.jpg);" href="">
-                    <div class="details">
-                        <h4>Portfolio 3</h4>
-                        <span>Alored dono par</span>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a class="portfolio-item" style="background-image: url(img/portfolio-4.jpg);" href="">
-                    <div class="details">
-                        <h4>Portfolio 4</h4>
-                        <span>Alored dono par</span>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a class="portfolio-item" style="background-image: url(img/portfolio-5.jpg);" href="">
-                    <div class="details">
-                        <h4>Portfolio 5</h4>
-                        <span>Alored dono par</span>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a class="portfolio-item" style="background-image: url(img/portfolio-6.jpg);" href="">
-                    <div class="details">
-                        <h4>Portfolio 6</h4>
-                        <span>Alored dono par</span>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a class="portfolio-item" style="background-image: url(img/portfolio-7.jpg);" href="">
-                    <div class="details">
-                        <h4>Portfolio 7</h4>
-                        <span>Alored dono par</span>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a class="portfolio-item" style="background-image: url(img/portfolio-8.jpg);" href="">
-                    <div class="details">
-                        <h4>Portfolio 8</h4>
-                        <span>Alored dono par</span>
-                    </div>
-                </a>
-            </div>
-
-        </div>
-    </div>
-</section>
-
-<!--==========================
-  Testimonials Section
-============================-->
-<section id="testimonials">
-    <div class="container wow fadeInUp">
-        <div class="row">
-            <div class="col-md-12">
-                <h3 class="section-title">Testimonials</h3>
-                <div class="section-title-divider"></div>
-                <p class="section-description">Erdo lide, nora porodo filece, salvam esse se, quod concedimus ses haec dicturum fuisse</p>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-3">
-                <div class="profile">
-                    <div class="pic"><img src="img/client-1.jpg" alt=""></div>
-                    <h4>Saul Goodman</h4>
-                    <span>Lawless Inc</span>
-                </div>
-            </div>
-            <div class="col-md-9">
-                <div class="quote">
-                    <b><img src="img/quote_sign_left.png" alt=""></b> Proin iaculis purus consequat sem cure  digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper. <small><img src="img/quote_sign_right.png" alt=""></small>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-9">
-                <div class="quote">
-                    <b><img src="img/quote_sign_left.png" alt=""></b> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis architecto beatae. <small><img src="img/quote_sign_right.png" alt=""></small>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="profile">
-                    <div class="pic"><img src="img/client-2.jpg" alt=""></div>
-                    <h4>Sara Wilsson</h4>
-                    <span>Odeo Inc</span>
-                </div>
-            </div>
-        </div>
-
-    </div>
-</section>
-
-<!--==========================
-  Team Section
-============================-->
-<section id="team">
-    <div class="container wow fadeInUp">
-        <div class="row">
-            <div class="col-md-12">
-                <h3 class="section-title">Our Team</h3>
-                <div class="section-title-divider"></div>
-                <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-3">
-                <div class="member">
-                    <div class="pic"><img src="img/team-1.jpg" alt=""></div>
-                    <h4>Walter White</h4>
-                    <span>Chief Executive Officer</span>
-                    <div class="social">
-                        <a href=""><i class="fa fa-twitter"></i></a>
-                        <a href=""><i class="fa fa-facebook"></i></a>
-                        <a href=""><i class="fa fa-google-plus"></i></a>
-                        <a href=""><i class="fa fa-linkedin"></i></a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3">
-                <div class="member">
-                    <div class="pic"><img src="img/team-2.jpg" alt=""></div>
-                    <h4>Sarah Jhinson</h4>
-                    <span>Product Manager</span>
-                    <div class="social">
-                        <a href=""><i class="fa fa-twitter"></i></a>
-                        <a href=""><i class="fa fa-facebook"></i></a>
-                        <a href=""><i class="fa fa-google-plus"></i></a>
-                        <a href=""><i class="fa fa-linkedin"></i></a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3">
-                <div class="member">
-                    <div class="pic"><img src="img/team-3.jpg" alt=""></div>
-                    <h4>William Anderson</h4>
-                    <span>CTO</span>
-                    <div class="social">
-                        <a href=""><i class="fa fa-twitter"></i></a>
-                        <a href=""><i class="fa fa-facebook"></i></a>
-                        <a href=""><i class="fa fa-google-plus"></i></a>
-                        <a href=""><i class="fa fa-linkedin"></i></a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3">
-                <div class="member">
-                    <div class="pic"><img src="img/team-4.jpg" alt=""></div>
-                    <h4>Amanda Jepson</h4>
-                    <span>Accountant</span>
-                    <div class="social">
-                        <a href=""><i class="fa fa-twitter"></i></a>
-                        <a href=""><i class="fa fa-facebook"></i></a>
-                        <a href=""><i class="fa fa-google-plus"></i></a>
-                        <a href=""><i class="fa fa-linkedin"></i></a>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </div>
-</section>
-
 <!--==========================
   Contact Section
 ============================-->
@@ -435,7 +138,7 @@
             <div class="col-md-12">
                 <h3 class="section-title">Contact Us</h3>
                 <div class="section-title-divider"></div>
-                <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
+                <p class="section-description">Because you want to </p>
             </div>
         </div>
 
@@ -444,17 +147,17 @@
                 <div class="info">
                     <div>
                         <i class="fa fa-map-marker"></i>
-                        <p>A108 Adam Street<br>New York, NY 535022</p>
+                        <p>Munich Maker Lab <br> Munich </p>
                     </div>
 
                     <div>
                         <i class="fa fa-envelope"></i>
-                        <p>info@example.com</p>
+                        <p>info@shipMeUp.com</p>
                     </div>
 
                     <div>
                         <i class="fa fa-phone"></i>
-                        <p>+1 5589 55488 55s</p>
+                        <p>+42 42 42 42</p>
                     </div>
 
                 </div>
